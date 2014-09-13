978e63f69670780b1035250ffe4d85ef782c7034
Mihnea Simian <contact@mesimian.com>
2014-09-13 15:08:47 +0300

    ask for profile update before adding article if email is missing
    This is because instagram, like twitter, doesn't
    provide user email address by API

--- public/modules/articles/config/articles.client.routes.js
+++ public/modules/articles/config/articles.client.routes.js
@@ -1,25 +1,36 @@
 'use strict';
 
 // Setting up route
-angular.module('articles').config(['$stateProvider',
-	function($stateProvider) {
+angular.module('articles').config(['$stateProvider', '$injector',
+	function($stateProvider, $injector) {
 		// Articles state routing
 		$stateProvider.
 		state('listArticles', {
 			url: '/articles',
-			templateUrl: 'modules/articles/views/list-articles.client.view.html'
+			templateUrl: 'modules/articles/views/list-articles.client.view.html',
 		}).
 		state('createArticle', {
 			url: '/articles/create',
-			templateUrl: 'modules/articles/views/create-article.client.view.html'
+			templateUrl: 'modules/articles/views/create-article.client.view.html',
+			resolve: {
+				'profile': function($q, $state,  Authentication) {
+					var defer = $q.defer();
+					if (!Authentication.user.email) {
+						defer.reject({reason: 'missing_email', 'next': 'createArticle'});
+					} else {
+						defer.resolve();
+					}
+					return defer.promise;
+				},
+			},
 		}).
 		state('viewArticle', {
 			url: '/articles/:articleId',
-			templateUrl: 'modules/articles/views/view-article.client.view.html'
+			templateUrl: 'modules/articles/views/view-article.client.view.html',
 		}).
 		state('editArticle', {
 			url: '/articles/:articleId/edit',
-			templateUrl: 'modules/articles/views/edit-article.client.view.html'
+			templateUrl: 'modules/articles/views/edit-article.client.view.html',
 		});
 	}
 ]);

07ffe010e9a92939c7dd64e2eae0225d7714c59a
Mihnea Simian <contact@mesimian.com>
2014-11-09 15:47:41 +0200

    scratching story views

---
+++ public/modules/stories/config/stories.client.routes.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// Setting up route
+angular.module('stories').config(['$stateProvider', '$injector',
+	function($stateProvider, $injector) {
+		// Stories state routing
+		$stateProvider.
+		state('listStories', {
+			url: '/stories',
+			templateUrl: 'modules/stories/views/list-stories.client.view.html',
+		}).
+		state('createStory', {
+			url: '/stories/create',
+			templateUrl: 'modules/stories/views/create-story.client.view.html',
+			resolve: {
+				'profile': function($q, $state,  Authentication) {
+					var defer = $q.defer();
+					if (!Authentication.user.email) {
+						defer.reject({reason: 'missing_email', 'next': 'createStory'});
+					} else {
+						defer.resolve();
+					}
+					return defer.promise;
+				},
+			},
+		}).
+		state('viewStory', {
+			url: '/stories/:storyId',
+			templateUrl: 'modules/stories/views/view-story.client.view.html',
+		}).
+		state('editStory', {
+			url: '/stories/:storyId/edit',
+			templateUrl: 'modules/stories/views/edit-story.client.view.html',
+		});
+	}
+]);

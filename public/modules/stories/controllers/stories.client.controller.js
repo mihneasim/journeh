da359e863c247af31500f8f94548eb5bffe152a9
Mihnea Simian <contact@mesimian.com>
2014-12-01 19:18:02 +0200

    stories controller create - pickable and sortable grams

--- public/modules/stories/controllers/stories.client.controller.js
+++ public/modules/stories/controllers/stories.client.controller.js
@@ -4,6 +4,52 @@ angular.module('stories').controller('StoriesController', ['$scope', '$statePara
 	function($scope, $stateParams, $location, Authentication, Stories, Grams) {
 		$scope.authentication = Authentication;
 
+		// CREATE
+		$scope.initCreate = function() {
+			$scope.step = 1;
+			$scope.grams = Grams.query();
+			$scope.selectedGrams = [];
+		};
+
+		$scope.goTo = function(step) {
+			$scope.step = step;
+		}
+
+		$scope.toggleGram = function(gram) {
+			var pos = $scope.selectedGrams.indexOf(gram);
+			if (pos !== -1) {
+				$scope.selectedGrams.splice(pos, 1);
+			} else {
+				$scope.selectedGrams.push(gram);
+			}
+		};
+
+		$scope.moveDown = function(gram) {
+			var pos = $scope.selectedGrams.indexOf(gram),
+				actual;
+			if (pos < $scope.selectedGrams.length - 1) {
+				actual = $scope.selectedGrams.splice(pos, 1);
+				$scope.selectedGrams.splice(pos + 1, 0, actual[0]);
+			}
+		}
+
+		$scope.moveUp = function(gram) {
+			var pos = $scope.selectedGrams.indexOf(gram),
+				actual;
+			if (pos > 0) {
+				actual = $scope.selectedGrams.splice(pos, 1);
+				$scope.selectedGrams.splice(pos - 1, 0, actual[0]);
+			}
+		}
+
+		$scope.compileBody = function() {
+			$scope.body = '';
+			_.each($scope.selectedGrams, function(item) {
+				$scope.body += item.caption + "\n";
+			});
+
+		}
+
 		$scope.create = function() {
 			var story = new Stories({
 				title: this.title,
@@ -47,10 +93,6 @@ angular.module('stories').controller('StoriesController', ['$scope', '$statePara
 
 		$scope.find = function() {
 			$scope.stories = Stories.query();
-		};
-
-		$scope.initCreate = function() {
-			$scope.grams = Grams.query();
 		};
 
 		$scope.findOne = function() {

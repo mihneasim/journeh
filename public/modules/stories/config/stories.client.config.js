742ee4ad3dfbdae2fdec1a3f916568142c235b61
Mihnea Simian <contact@mesimian.com>
2015-01-03 17:36:58 +0200

    menu configs

--- public/modules/stories/config/stories.client.config.js
+++ public/modules/stories/config/stories.client.config.js
@@ -4,8 +4,13 @@
 angular.module('stories').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Stories', 'stories', 'dropdown', '/stories(/create)?');
-		Menus.addSubMenuItem('topbar', 'stories', 'List stories', 'stories');
-		Menus.addSubMenuItem('topbar', 'stories', 'New Story', 'stories/create');
+		// Cancel this ..
+		//Menus.addMenuItem('topbar', 'Stories', 'stories', 'dropdown', '/stories(/create)?');
+		//Menus.addSubMenuItem('topbar', 'stories', 'List stories', 'stories');
+		//Menus.addSubMenuItem('topbar', 'stories', 'New Story', 'stories/create');
+		// And try this:
+		Menus.addMenuItem('topbar', 'Explore Stories', 'stories');
+		Menus.addMenuItem('topbar', 'New Story', 'stories/create', 'item', '/stories/create', false);
+
 	}
 ]);

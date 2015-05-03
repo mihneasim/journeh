1764f93ba6cdf78c854027cfd70728a91478e8fd
Mihnea Simian <contact@mesimian.com>
2015-05-03 20:53:23 +0300

    filters should fail silently

--- public/modules/core/config/core.client.config.js
+++ public/modules/core/config/core.client.config.js
@@ -4,9 +4,15 @@ angular.module('core')
 .value('CDN', {'journeh': 'd1cda9fzfodqce.cloudfront.net'})
 .filter('cdnize', ['CDN', function(CDN) {
     return function (input) {
-        var bucketSearch = input.match(/https?:\/\/([^\/]+)\/([^\/]+)\//),
+        var bucketSearch,
             domain,
             bucket;
+
+        if (!input) {
+            return input;
+        }
+
+        bucketSearch = input.match(/https?:\/\/([^\/]+)\/([^\/]+)\//);
 
         if (bucketSearch === null) {
             return input;
